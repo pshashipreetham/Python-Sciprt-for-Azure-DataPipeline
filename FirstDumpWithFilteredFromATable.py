dc24bb9a2423a3482c364e2647a00c7679e21fda
psp316r <preetham.p1997@gmail.com>
2021-08-23 21:19:44 +0530

    Fixed Appending Issue

--- FirstDumpWithFilteredFromATable.py
+++ FirstDumpWithFilteredFromATable.py
@@ -15,16 +15,16 @@ batch_size = 100
 file_split_size = 500
 
 """Step-1: Connect to the DB """
-conn = psycopg2.connect("host=s1-d-war-pgs-01.postgres.database.azure.com "
-                        "dbname=thingworx "
-                        "user=shashi@s1-d-war-pgs-01 "
-                        "password='shashi'")
+conn = psycopg2.connect("host=********* "
+                        "dbname=********* "
+                        "user=********* "
+                        "password='*********'")
 cur = conn.cursor()
 conn.commit()
 
 """Step-2: Get the number of things from DB """
-# cur.execute("Select Distinct source_id from value_stream;")
-data = [('HP-2',), ('Temp_TestPump_Mayank',), ('HP-4',), ('SMFTank001',), ('TempRunhours_Drishya',), ('LVTank206',), ('LVTank108',), ('HP-3',), ('HP-2_PumpWarGames_',), ('LVTank110',), ('LVTank103',), ('LVPump500',), ('DataChangeThing',), ('HP-4_PumpWarGames',), ('LVPump400',), ('HP-3_PumpWarGames',), ('LVTank102',), ('LVTank201',), ('LVTank109',), ('HP-3_PumpWarGames_',), ('Test_KepwareLV_Thing',), ('Tank-101',), ('ST-3',), ('OakKepwareThing',), ('LVTank203',), ('ST-1',), ('LVPump200',), ('OakO&M_WarGames',), ('HP-4_PumpWarGames_',), ('Hp1_warGame',), ('LVTank101',), ('ST-2_TankWarGame',), ('Test_HP-4',), ('PumpWarThing',), ('LVTank111',), ('ST-2',), ('LVTank106',), ('HP-1_PumpWarGames',), ('OakO&M',), ('Temp_LVPump400_Divya',), ('LVTank204',), ('LVO&M',), ('LVKepwareThing',), ('HP-2_PumpWarGames',), ('LVM',), ('LVPump100',), ('Tank101',), ('HP-1_PumpWarGames_new',), ('WarOakKepwareThing',), ('Temp_PumpWarThing_Divya',), ('LVTank205',), ('HP-1',), ('TankThingWar',), ('LVTank107',), ('LVPump600',), ('LVTank202',), ('ST-1_TankWarGame',), ('OandMWarThingOakland',), ('LVPump300',), ('TestKepwareThing',), ('ST-3_TankWarGame',), ('DataChange',), ('Test_HP-1',)];
+cur.execute("Select Distinct source_id from value_stream;")
+data = cur.fetchall();
 print(data)
 
 """ Function that appends the data to the Blobs in Container """
@@ -33,13 +33,13 @@ print(data)
 def load_to_azure2(filename, sourceid):
     flag = False
     blob_service_client = BlobServiceClient.from_connection_string(
-        "DefaultEndpointsProtocol=https;AccountName=saimiotanalytics;AccountKey=PNSTNeS3fAMZZelHblfJUHqnOzksBNGu2oI0BvpGlt97X30iR79izGOWpSL8VuYuOzMrqmeu+TY7vXirIUidPQ==;EndpointSuffix=core.windows.net")
+        "DefaultEndpointsProtocol=https;AccountName=*********;AccountKey=*********;EndpointSuffix=core.windows.net")
     service_client = DataLakeServiceClient(
-        account_url="{}://{}.dfs.core.windows.net".format("https", "saimiotanalytics"),
-        credential="PNSTNeS3fAMZZelHblfJUHqnOzksBNGu2oI0BvpGlt97X30iR79izGOWpSL8VuYuOzMrqmeu+TY7vXirIUidPQ==")
-    file_system_client = service_client.get_file_system_client(file_system="test-data")
+        account_url="{}://{}.dfs.core.windows.net".format("https", "*********"),
+        credential="*********")
+    file_system_client = service_client.get_file_system_client(file_system="*********")
     # Get the Blob Names from the Container
-    container_client = blob_service_client.get_container_client("test-data")
+    container_client = blob_service_client.get_container_client("*********")
     blobs_list = container_client.list_blobs()
     # Check the Blob name is present or not
     for blob in blobs_list:
